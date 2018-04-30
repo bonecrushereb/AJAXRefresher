425e403e830a52c94963022029512a7ca0972ea8
bonecrushereb <bonecrushereb@gmail.com>
2018-04-30 11:36:20 -0700

    moved pageToken outside of document.ready function equal to empty object

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-var pageToken = '';
+var pageToken = {};
 $(document).ready(function() {
   $('#searchButton').click(function() {
     searchYoutube();
   });
-  $('.tokenClass').off('click').click(function() {
+  $('.tokenClass').click(function() {
     pageToken.current = $(this).val() == 'next' ? pageToken.nextPage : pageToken.prevPage;
-    console.log(pageToken.nextPage);
+    console.log(pageToken.current);
     searchYoutube();
   });
 });
 
 function searchYoutube() {
-  var pageToken = {};
   $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/search',
     dataType: 'json',
@@ -24,18 +23,54 @@ function searchYoutube() {
       pageToken: pageToken.current
     }
   }).done(function(data) {
-    console.log(data);
-    // console.log(data.nextPageToken);
     pageToken.nextPage = data.nextPageToken;
     pageToken.prevPage = data.prevPageToken;
+    console.log(pageToken);
     var html = '';
     $.each(data['items'], function(idx, val) {
-      // console.log(val.id);
       html+='<div class="title">' + val.snippet.title + '</div>';
       html+='<div class="url"><a href="https://www.youtube.com/watch?v=' + val.id.videoId + '" target="_blank">' + val.id.videoId + '</a></div>';
       html+='<img class="thumbnail" src="' + val.snippet.thumbnails.medium.url + '" />';
     });
+    // console.log(pageToken.current);
     $('#output').html(html);
   });
 };
 
+  // var pageToken = {};
+  //      $(document).ready(function(){
+  //          $('#searchButton').click(function(){
+  //              searchYoutube();
+  //          })
+  //          $('.tokenClass').click(function(){
+  //              pageToken.current = $(this).val()=='next' ? pageToken.nextPage : pageToken.prevPage;
+  //              searchYoutube();
+  //          })
+  //      })
+        
+  //       function searchYoutube(){
+  //           $.ajax({
+  //               url:'https://www.googleapis.com/youtube/v3/search',
+  //               dataType:'json',
+  //               type:'GET',
+  //               data:{
+  //                   key:'AIzaSyCoRaVE9MNlID2C-iieNNwC19l6_uTQtv0',
+  //                   q:$('#search').val(),
+  //                   part:'snippet',
+  //                   maxResults:10,
+  //                   pageToken: pageToken.current
+  //               }
+  //           }).done(function(data){
+  //               pageToken.nextPage = data.nextPageToken;
+  //               pageToken.prevPage = data.prevPageToken;
+  //               console.log(pageToken)
+  //               var html = "";
+  //               $.each(data['items'],function(index,value){
+  //                   html += '<div><div class="title">'+value.snippet.title+'</div>';
+  //                   html += '<div><div class="url"><a href="https://www.youtube.com/watch?v='+value.id.videoId+'" target="_blank">'+value.id.videoId+'</a></div>';
+  //                   html += '<div><img  class="thumbnail" src="'+value.snippet.thumbnails.medium.url+'"></div>';
+  //                   html += '</div>';
+  //               })
+  //               $('#output').html(html);
+  //           })
+  //       }
