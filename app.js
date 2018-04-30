5f45cd06d5581e45adb8437ce5be12b3b00d89f8
bonecrushereb <bonecrushereb@gmail.com>
2018-04-30 00:13:48 -0700

    can not figure out nextpageToken

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
+var pageToken = '';
 $(document).ready(function() {
   $('#searchButton').click(function() {
+    searchYoutube();
+  });
+  $('.tokenClass').off('click').click(function() {
+    pageToken.current = $(this).val() == 'next' ? pageToken.nextPage : pageToken.prevPage;
+    console.log(pageToken.nextPage);
     searchYoutube();
   });
 });
 
 function searchYoutube() {
+  var pageToken = {};
   $.ajax({
     url: 'https://www.googleapis.com/youtube/v3/search',
     dataType: 'json',
     type: 'GET',
     data: {
       key: 'AIzaSyCoRaVE9MNlID2C-iieNNwC19l6_uTQtv0',
-      q: 'test',
+      q: $('#search').val(),
       part: 'snippet',  
-      maxResults: 25,
-      pageToken: 'CDIQAA'
+      maxResults: 10,
+      pageToken: pageToken.current
     }
   }).done(function(data) {
     console.log(data);
-    var nextPage = data.nextPageToken;
-    var prebPage = data.prevPageToken;
+    // console.log(data.nextPageToken);
+    pageToken.nextPage = data.nextPageToken;
+    pageToken.prevPage = data.prevPageToken;
     var html = '';
     $.each(data['items'], function(idx, val) {
+      // console.log(val.id);
       html+='<div class="title">' + val.snippet.title + '</div>';
-      html+='<div class="url">' + val.id.videoId + '</div>';
+      html+='<div class="url"><a href="https://www.youtube.com/watch?v=' + val.id.videoId + '" target="_blank">' + val.id.videoId + '</a></div>';
       html+='<img class="thumbnail" src="' + val.snippet.thumbnails.medium.url + '" />';
-      console.log(val);
     });
     $('#output').html(html);
   });
 };
+
